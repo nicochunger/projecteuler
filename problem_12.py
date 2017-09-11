49e99efd1b836e9575502bf440e5dcd7b1c21d4d
Nicolas Unger <nicounger94@gmail.com>
2017-09-11 09:48:25 -0300

    Solved problem 12 and 16.

--- problem_12.py
+++ problem_12.py
@@ -5,9 +5,11 @@ def check_divisors(n):
     assert isinstance(n, int)
 
     count = 1
-    for i in range(1,n/2+1):
+    for i in range(1,int(n**(0.5))+1):
         if n%i == 0:
             count += 1
+            if i != n/i:
+                count +=1
 
     return count
 
